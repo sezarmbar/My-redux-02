727fe902aea80a1cba96d95059f0e34cbf5ce4e1
sezarmbar <sezar_mbar@yahoo.com>
2016-12-22 17:20:29 +0100

    start

---
+++ src/components/googleMaps.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { withGoogleMap } from "react-google-maps";
+
+class componentName extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default componentName;
